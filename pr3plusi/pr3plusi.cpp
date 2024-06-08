47b26adc772df2e1ebb4141d0a9e755a96a709cc
Lagunoblvde <17p9lmblepyku2k19@gmail.com>
2024-06-08 10:54:05 +0300

    Добавьте файлы проекта.

---
+++ pr3plusi/pr3plusi.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    setlocale(0, "");
+
+    const int rows = 3, columns = 5;
+    int arr[rows][columns];
+
+    for (int i = 0; i < rows; i++) {
+        
+        for (int j = 0; j < columns; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < rows; i++) {
+        int stroka = 0;
+        for (int j = 0; j < columns; j++) {
+            cout << arr[i][j] << "\t";
+            stroka += arr[i][j];
+        }
+        double srednee = stroka / columns;
+        cout << "Среднее значение: " << srednee << endl;
+    }
+    return 0;
+}
